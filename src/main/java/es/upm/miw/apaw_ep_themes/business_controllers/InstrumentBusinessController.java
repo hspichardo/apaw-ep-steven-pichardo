637b145bd134b6fe721464d6e05accdb90769bc0
steven <hspichardo@gmail.com>
2019-10-10 10:52:17 +0200

    creating api_controller for instrument #1

---
+++ src/main/java/es/upm/miw/apaw_ep_themes/business_controllers/InstrumentBusinessController.java
@@ -0,0 +1,10 @@
+package es.upm.miw.apaw_ep_themes.business_controllers;
+
+import es.upm.miw.apaw_ep_themes.dtos.InstrumentBasicDto;
+import es.upm.miw.apaw_ep_themes.dtos.InstrumentCreationDto;
+
+public class InstrumentBusinessController {
+public InstrumentBasicDto create(InstrumentCreationDto instrumentCreationDto){
+    return null;
+}
+}
